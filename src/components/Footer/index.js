e50d17f5962023e013418d690c726a1de3bd255b
mohammed boussaada <91814930+mohamed0228@users.noreply.github.com>
2023-11-01 19:16:48 +0000

    changing footer icon

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,22 @@
 import React from "react";
-
+import {TbPhoneCall} from 'react-icons/tb'
+import {MdEmail} from 'react-icons/md'
+import {AiFillLinkedin,AiOutlineGithub} from 'react-icons/ai'
 function Footer(){
     return(
         <footer className="footer  fixed-bottom">
-        <a href="https://github.com/mohamed0228"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Github" className="icon"/></a>   
-        <a href="https://www.linkedin.com/in/mohamed0228/"><img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="LinkedIn" className="icon"/></a> 
-        <a href="mailto:mohamed0228@outlook.fr?"><img src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-email-notifications-justicon-flat-justicon.png"  alt="email" className="icon"/></a>
-        <a href="tel:2013440925"><img src="https://img.icons8.com/external-flat-juicy-fish/60/000000/external-phone-contact-us-flat-flat-juicy-fish-2.png" alt="Github" className="icon"/></a> 
+            <div style={{width:"60px",height:"60px",margin:"10px"}}>
+                <a href="tel:2013440925" style={{width:"60px",height:"60px"}}><TbPhoneCall size={50}  color="black"/></a> 
+            </div>
+            <div style={{width:"60px",height:"60px",margin:"10px"}}>
+                <a href="https://github.com/mohamed0228 "  style={{width:"60px",height:"60px"}}><AiOutlineGithub size={50}  color="black" /></a>   
+            </div>
+            <div style={{width:"60px",height:"60px",margin:"10px"}}>
+                <a href="mailto:mohamed0228@outlook.fr?" style={{width:"60px",height:"60px"}}><MdEmail size={50}  color="black" /></a>
+            </div>
+            <div style={{width:"60px",height:"60px",margin:"10px"}}>
+                <a href="https://www.linkedin.com/in/mohamed0228/" style={{width:"60px",height:"60px"}}><AiFillLinkedin size={50}  color="black"/></a> 
+            </div>
         </footer>
     )
 }
